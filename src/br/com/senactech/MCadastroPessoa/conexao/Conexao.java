ff30ebff609049b2a4757c72106dec397d98b762
Jair Ferraz <jairbferraz@outlook.com>
2022-06-10 18:16:54 +0100

    Classe Conexao

--- src/br/com/senactech/MCadastroPessoa/conexao/Conexao.java
+++ src/br/com/senactech/MCadastroPessoa/conexao/Conexao.java
@@ -5,10 +5,31 @@
  */
 package br.com.senactech.MCadastroPessoa.conexao;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.DriverManager;
 /**
  *
  * @author jairb
  */
 public class Conexao {
+    //cria uma constante com endereço do BD e schema
+    private static String url = "jdbc:mysql://localhost:3306/devm211";
+    //cria uma constante com o user de conexão do banco
+    private static String user = "root";
+    //cria uma constante com a senha de acesso ao BD
+    private static String pass = "";
     
+    public static Connection getConexao() throws SQLException{
+        //Iniciar conexão nula, ainda não estabelecida
+        Connection c = null;
+        //tenta estabelecer conexão
+        try {
+            c = DriverManager.getConnection(url, user, pass);
+            //Caso haja falha na conexão gera uma exceção
+        } catch (SQLException e) {
+            throw new SQLException("Erro ao conectar! \n" + e.getMessage());
+        }
+        return c;
+    }
 }
